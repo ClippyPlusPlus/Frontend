069b115ce3aaa22b0b638b091c5086f2109cb590
Laurens Groeneveld <laurens.groeneveld@infi.nl>
2020-11-06 19:37:14 +0100

    secure websocket

--- src/App.vue
+++ src/App.vue
@@ -66,7 +66,7 @@ export default {
     },
 
     initializeClient() {
-      this.client = mqtt.connect('ws://mqtt.eclipse.org:80/mqtt');
+      this.client = mqtt.connect('wss://mqtt.eclipse.org:443/mqtt');
       this.client.subscribe("clippy-plus-plus/play");
 
       this.client.on("message", function (topic, payload) {
