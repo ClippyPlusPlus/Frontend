91ed69f8206ad34b03b1430a1dc911ff1c04840b
Laurens Groeneveld <laurens.groeneveld@infi.nl>
2020-11-06 11:16:50 +0100

    Scaffold knop

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+     <div class="max-w-xl mx-auto pt-6">
+        <div class="inline-block bg-blue-700 p-3 text-blue-100 text-xl font-medium">
+          Clippy++
+        </div>
+    </div>
     <div id="nav">
       <!-- <router-link to="/">Home</router-link> -->
     </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Clippy++
+  <div class="max-w-xl mx-auto py-6">
+    <div @click="sendMessage('applause')" class="cursor-pointer">
+      Knopje
+    </div>
   </div>
 </template>
 
@@ -8,5 +10,21 @@
 // @ is an alias to /src
 export default {
   name: 'Home',
+
+  data() {
+    return {
+      client: null,
+    }
+  },
+
+  created() {
+    this.client = {};
+  },
+ 
+  methods: {
+    sendMessage() {
+      console.log("message");
+    }
+  }
 }
 </script>
