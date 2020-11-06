459307da8c40bfa888460ed3a80bcd3bbae35523
Rutger van Willigen <rutger.van.willigen@infi.nl>
2020-11-06 17:58:49 +0100

    Extract header component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <template>
   <div id="app">
-     <div class="max-w-5xl mx-auto px-6 pt-6 flex items-center justify-between">
-        <div class="inline-block bg-orange-500 p-3 text-white text-xl font-medium rounded">
-          Clippy++
-        </div>
-        <div>
-          <span class="text-gray-700 text-sm uppercase font-medium">
-            Status:
-          </span>
-          <span class="bg-gradient-to-r from-pink-400 to-orange-500 text-gray-900 p-2 rounded-full">
-            {{ status }}
-          </span>
-        </div>
-        <div class="bg-gray-200 rounded px-3 py-2 flex items-center space-x-2">
-            <span class="flex h-4 w-4 relative">
-              <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-              <span class="relative inline-flex rounded-full h-4 w-4 bg-green-500"></span>
-            </span>
-          <div>
-            Live
-          </div>
-        </div>
-    </div>
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> -->
-    </div>
-
-    <router-view/>
+    <Header 
+      v-bind:clientIsConnected=this.clientIsConnected
+      v-bind:messageCount=this.messageCount
+    />
 
     <div class="max-w-5xl mx-auto px-6 pt-6" v-if="loading">
         <svg class="animate-spin h-32 w-32 text-blue-500 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -34,53 +11,74 @@
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
     </div>
+
+    <Home :initialClient=this.client />
   </div>
 </template>
 
 <script>
 import { MESSAGE_TIMEOUT } from './utils/constants'
+import mqtt from 'mqtt'
+import Header from './components/Header.vue'
+import Home from './pages/Home.vue'
+import { playSound } from './utils/soundPlayer'
 
 export default {
+  name: "App",
+
+  components: {
+    Header,
+    Home
+  },
+
   data() {
     return {
       loading: true,
       messageCount: 0,
+      client: null,
+      clientIsConnected: false
     }
   },
+
   created() {
     this.$eventBus.$on('messageSent', this.increaseMessageCount);
     const messageCount = localStorage.getItem('messageCount');
     this.messageCount = messageCount;
+    this.initializeClient();
   },
+
   mounted() {
     if (window.location.hostname === 'localhost') {
       this.loading = false;
       return;
     }
 
-      const timeout = setTimeout(() => {
-        this.loading = false;
-        clearTimeout(timeout);
-      }, MESSAGE_TIMEOUT);
-  },
-
-  computed: {
-    status() {
-      if (this.messageCount >= 50) { return "Crazy" }
-      if (this.messageCount >= 30) { return "Verslaafde" }
-      if (this.messageCount >= 20) { return "Grootmeester" }
-      if (this.messageCount >= 10) { return "Professional" }
-      if (this.messageCount >= 5) { return "Poweruser" }
-      if (this.messageCount >= 3) { return "Starter" }
-      return "Newbie";
-    },
+    const timeout = setTimeout(() => {
+      this.loading = false;
+      clearTimeout(timeout);
+    }, MESSAGE_TIMEOUT);
   },
 
   methods: {
-      increaseMessageCount () {
-        this.messageCount++;
-        localStorage.setItem('messageCount', this.messageCount);
-      }
+    increaseMessageCount () {
+      this.messageCount++;
+      localStorage.setItem('messageCount', this.messageCount);
+    },
+
+    initializeClient() {
+      this.client = mqtt.connect('ws://mqtt.eclipse.org:80/mqtt');
+      this.client.subscribe("clippy-plus-plus/play");
+
+      this.client.on("message", function (topic, payload) {
+        var receivedObject = JSON.parse(payload);
+        playSound(receivedObject);
+      });
+
+      this.client.on("connect", function() {
+        console.log("connected");
+        this.clientIsConnected = true;
+      });
+    }
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="max-w-5xl mx-auto px-6 pt-6 flex items-center justify-between">
+    <div class="inline-block bg-orange-500 p-3 text-white text-xl font-medium rounded">
+      Clippy++
+    </div>
+    <div>
+      <span class="text-gray-700 text-sm uppercase font-medium">
+        Status:
+      </span>
+      <span class="bg-gradient-to-r from-pink-400 to-orange-500 text-gray-900 p-2 rounded-full">
+        {{ status }}
+      </span>
+    </div>
+    <div class="bg-gray-200 rounded px-3 py-2 flex items-center space-x-2">
+        <span class="flex h-4 w-4 relative">
+          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+          <span class="relative inline-flex rounded-full h-4 w-4 bg-green-500"></span>
+        </span>
+      <div>
+        {{ this.clientIsConnected ? "Live" : "Disconnected" }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  props: [ "clientIsConnected", "messageCount" ],
+
+  computed: {
+    status() {
+      if (this.messageCount >= 50) { return "Crazy" }
+      if (this.messageCount >= 30) { return "Verslaafde" }
+      if (this.messageCount >= 20) { return "Grootmeester" }
+      if (this.messageCount >= 10) { return "Professional" }
+      if (this.messageCount >= 5) { return "Poweruser" }
+      if (this.messageCount >= 3) { return "Starter" }
+      return "Newbie";
+    },
+  }
+}
+</script>
